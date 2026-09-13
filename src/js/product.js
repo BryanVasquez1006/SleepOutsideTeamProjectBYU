a3f322d082ce7621190588f52b8c7d8d578492c6
BryanVasquez1006 <bryan.vasqueziraheta@gmail.com>
2026-09-12 21:54:59 -0600

    completed week 02 team activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -6,24 +6,3 @@ const dataSource = new ProductData("tents");
 const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-
-console.log(dataSource.findProductById(productId));
-console.log(productId);
-
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-  
-
-
